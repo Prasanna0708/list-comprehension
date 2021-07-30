d4f0032dd101b6376a303f364b31b54775612451
Prasanna0708 <78546913+Prasanna0708@users.noreply.github.com>
2021-07-30 14:35:28 +0530

    Add files via upload

---
+++ 15-06TaskListComprehension.py
@@ -0,0 +1,14 @@
+#list comprehension using list
+x = "Python is aprogramming Language"
+y = []
+for item in x:
+    y.append(item)
+
+print(y)
+
+#list comprehension using tuples
+a = "Tuples is a Data structures in Python"
+b = set((a))
+for item in a:
+    continue
+    print(b)
